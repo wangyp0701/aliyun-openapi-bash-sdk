df457900e79972ebfe99bc9b1ca135e4aa773132
wangyunpeng <33682005+wangyp0701@users.noreply.github.com>
2024-06-04 15:06:19 +0800

    Add files via upload
    全站加速证书配置 DCDN

--- examples/UpdateSSLCert.sh
+++ examples/UpdateSSLCert.sh
@@ -7,10 +7,10 @@
 # 可配合 acme.sh 使用的 renewHook 脚本：自动将新证书上传至阿里云并更新对应 CDN 域名，然后删除对应域名的旧证书。
 # 每次 API 执行都会检测是否失败，如果失败，会中断脚本执行并返回自定义错误代码。
 
-AliAccessKeyId="<AliAccessKeyId>"
-AliAccessKeySecret="<AliAccessKeySecret>"
+AliAccessKeyId=""
+AliAccessKeySecret=""
 # shellcheck source=AliyunOpenApiSDK.sh
-source ../AliyunOpenApiSDK.sh
+source /opt/aliyun-openapi-bash-sdk/AliyunOpenApiSDK.sh
 
 # acme.sh 执行 renewHook 时导出的环境变量列表
 ACME_ENV_LIST=(
@@ -42,7 +42,7 @@ DOMAIN=$Le_Domain
 CERT_NAME="${DOMAIN//./_}-$(date +%s)"
 # 需要更新证书的 CDN 域名列表
 DOMAIN_LIST=(
-    "example.example.com"
+    "image-slef.yappam.com"
 )
 
 # 获取证书列表
@@ -51,16 +51,26 @@ result=$(aliapi_rpc GET cas.aliyuncs.com  2018-07-13 DescribeUserCertificateList
 cert_list=$(jq -cr ".CertificateList|map(select(.common == \"$DOMAIN\"))|map(.id)|.[]" <<< "$result")
 
 # 上传新的证书
-aliapi_rpc GET cas.aliyuncs.com 2018-07-13 CreateUserCertificate --Cert "get_cert()" --Key "get_key()" --Name "$CERT_NAME" || exit 102
+#aliapi_rpc GET cas.aliyuncs.com 2018-07-13 CreateUserCertificate --Cert "get_cert()" --Key "get_key()" --Name "$CERT_NAME" || exit 102
 
+cdn_set(){
 # 设置 CDN 域名列表使用新的证书
 for _domain in "${DOMAIN_LIST[@]}"; do
     aliapi_rpc GET cdn.aliyuncs.com 2018-05-10 SetDomainServerCertificate --DomainName "$_domain" --ServerCertificateStatus on --CertName "$CERT_NAME" --CertType cas || exit 103
 done
 unset _domain
-
+}
 # 删除旧的证书
 for _id in ${cert_list}; do
     aliapi_rpc GET cas.aliyuncs.com 2018-07-13 DeleteUserCertificate --CertId "$_id" || exit 104
 done
 unset _id
+
+#查询证书id
+#	aliapi_rpc GET cas.aliyuncs.com 2020-04-07 ListUserCertificateOrder --Keyword "$_domain" --OrderType upload|jq .CertificateOrderList.CertificateId
+
+
+# 设置 DCDN 域名列表使用新的证书
+for _domain in "${DOMAIN_LIST[@]}"; do
+	aliapi_rpc GET dcdn.aliyuncs.com 2018-01-15 SetDcdnDomainSSLCertificate --DomainName "$_domain" --CertType upload --SSLProtocol on  --SSLPub "get_cert()" --SSLPri "get_key()"|| exit 103
+    done
